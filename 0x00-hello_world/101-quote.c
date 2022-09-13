4bc9d2c537827a83c874c9ca59d506201895aea3
Callkehinde <callkehindealways2@gmail.com>
2022-09-12 20:58:35 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -14,8 +14,8 @@
 int main(void)
 {
   write(2,
-          "and that piece of art useful\"- Dora Korpar, 2015-10-19\n",
-	  59);
+          "and that piece of art useful\" - Dora Korpar, 2015-10-19\n",
+59);
 
   return (1);
 }
