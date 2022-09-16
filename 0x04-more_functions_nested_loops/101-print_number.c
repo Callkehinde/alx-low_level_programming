b6302cf1a780b58b762a7c8a83061822b335faf3
Callkehinde <callkehindealways2@gmail.com>
2022-09-16 16:19:43 -0700

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,7 +3,7 @@
 /**
  * print_number - checks for checks for a digit (0 through 9)
  * @n: n - Variable
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 void print_number(int n)
 {
@@ -14,13 +14,13 @@ void print_number(int n)
 
 	if (n < 10 && n >= 0)
 	{
-		putchar (n + '0');
+		_putchar (n + '0');
 	}
 	else if (n > -10 && n < 0)
 	{
 		n = n - 2 * n;
-		putchar('_');
-		putchar (n + '0');
+		_putchar('_');
+		_putchar (n + '0');
 	}
 
 	else
@@ -28,7 +28,7 @@ void print_number(int n)
 		if (n < 0)
 		{
 			n = n * -1;
-			putchar ('_');
+			_putchar ('_');
 		}
 		z = n;
 		while (z / b > 9)
@@ -39,7 +39,7 @@ void print_number(int n)
 		{
 			m = z / b;
 			z = z % b;
-			putchar (m + '0');
+			_putchar (m + '0');
 			b = b / 10;
 		}
 	}
