b18778e61b9405d71ae87b1d320370338b7a22d6
Callkehinde <callkehindealways2@gmail.com>
2022-09-20 01:02:42 -0700

    hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,13 @@
-/*
- * File: 101-quote.c
- * Auth: Abdul-Lateef Kehnde
- */
-
-#include <unistd.h>
+#include <stdio.h>
 
 /**
- * main - Prints "and that piece of art is useful" Dora Korpar, 2015-10-19",
- * followed by a new line, to standard error.
+ * main - Entry point
  *
- * Return: Always 1
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
 	return (1);
 }
