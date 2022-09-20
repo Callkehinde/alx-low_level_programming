8e87130c59b1549c8375eaaa2ba4f16c38e50e93
Callkehinde <callkehindealways2@gmail.com>
2022-09-20 07:57:08 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,23 +10,26 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 99; i++)
+	for (i = 0; i <= 98; i++)
 	{
-		for (j = i + 1; j < 100; i++)
+		for (j = i + 1; j <= 99; i++)
 		{
-			putchar(i / 10 + '0');
-			putchar(i % 10 + '0');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
 			putchar(' ');
-			putchar(j / 10 + '0');
-			putchar(j % 10 + '0');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
 			
-			if (!(i / 10 == 9 && i % 10 == 8))
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			if (i == 98 && j == 99)
+				break;
+			
+			putchar(',');
+			putchar(' ');
 		}
 	}
+
+
 	putchar('\n');
+
 	return (0);
 }
