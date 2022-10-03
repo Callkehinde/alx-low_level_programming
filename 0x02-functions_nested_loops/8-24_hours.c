1580bdd0dfd03ab123aa6a2fb9ee4a4f6931a33f
Callkehinde <callkehindealways2@gmail.com>
2022-10-03 16:13:58 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Function that prints every minute of the day
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int m;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(m / 10 +'0');
+			_putchar(m % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
+
