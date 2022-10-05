fc72f4e1629844d6d4bcb3efe3f97deb91e7eca4
Callkehinde <callkehindealways2@gmail.com>
2022-10-04 23:37:00 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 
 /**
  * _abs - Function that computes the absolute value of an integer
- * @n: the parameter of the function 
+ * @n: the parameter of the function
  * Return: the absolute value of n
  */
 
