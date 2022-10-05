f2137b0f36a30354eeb7a00f1c2ca823a22e783a
Callkehinde <callkehindealways2@gmail.com>
2022-10-05 07:18:10 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of this program
+ * @argc: Number of arguments passed to the function
+ * @argv: Argument vector of pointers to strings
+ *
+ * Return: Always 0
+ */
+
+int main(int argc_attribute_((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
