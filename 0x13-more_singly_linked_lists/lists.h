ee73667fba8e91161df41efa171897fae95f28dd
Callkehinde <callkehindealways2@gmail.com>
2022-10-26 18:11:38 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -24,13 +24,13 @@ listint_t *add_nodeint_end(listint_t **head, const int);
 void free_listint(listint_t *);
 void free_listint2(listint_t **);
 int pop_listint(listint_t **);
-listint_t *get_nodeint_at_index(listint_t *, unsigned int);
-int sum_listint(listint_t *);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-listint_t *reverse_listint(listint_t **);
-size_t print_listint_safe(const listint_t *);
-size_t free_listint_safe(listint_t **);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
 #endif
