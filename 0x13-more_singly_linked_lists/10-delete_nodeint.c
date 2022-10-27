0caf0563ebfefb2e58b0e474bdf7b9240ee37552
Callkehinde <callkehindealways2@gmail.com>
2022-10-27 05:11:23 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,17 +11,16 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp = *head;
-	listint_t current = NULL;
+	listint_t *current = NULL;
+
 	unsigned int i = 0;
 
 	if (*head == NULL)
 		return (-1);
-
 	if (index == 0)
 	{
 		*head = (*head)->next;
 		free(temp);
-		*head = next;
 		return (1);
 	}
 
@@ -36,5 +35,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	current = temp->next;
 	temp->next = current->next;
 	free(current);
+
 	return (1);
 }
