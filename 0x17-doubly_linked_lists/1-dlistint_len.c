7eb04d49e05fe8c17a0ca9fae6e822b2fe0a0f39
Callkehinde <callkehindealways2@gmail.com>
2022-12-08 13:58:12 -0800

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -10,12 +10,9 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	int count;
-	
 	count = 0;
-	
 	if (h == NULL)
 		return (count);
-	
 	while (h->prev != NULL)
 		h = h->prev;
 	
@@ -24,6 +21,5 @@ size_t dlistint_len(const dlistint_t *h)
 		count++;
 		h = h->next;
 	}
-	
 	return (count);
 }
